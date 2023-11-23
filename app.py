706d5677caedb60d3c0c6d2ccc1ab1f58bd8038b
karlanrobles <karlanatalyrobles@gmail.com>
2023-11-22 19:20:01 -0600

    user input in first tab, opinions displayed in second tab

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template, request, session
-import secrets
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.secret_key = secrets.token_hex(24)
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
-def index():
-    if 'user_inputs' not in session:
-        session['user_inputs'] = []
-    return render_template('index.html', user_inputs=session['user_inputs'])
+def input_page():
+    return render_template('input_page.html')
 
-@app.route('/save_input', methods=['POST'])
-def save_input():
+@app.route('/process_input', methods=['POST'])
+def process_input():
     user_input = request.form.get('userInput')
     if user_input.strip():
+        session['user_inputs'] = session.get('user_inputs', [])  # Initialize list if not present
         session['user_inputs'].append(user_input)
-    return render_template('index.html', user_inputs=session['user_inputs'])
+        session.modified = True
+
+    return redirect(url_for('input_page'))
+
+@app.route('/display_page')
+def display_page():
+    user_inputs = session.get('user_inputs', [])
+    return render_template('display_page.html', user_inputs=user_inputs)
 
 if __name__ == '__main__':
     app.run(debug=True)
